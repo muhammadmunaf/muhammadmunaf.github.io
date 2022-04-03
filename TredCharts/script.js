4d5b4a14a07c3991f588074db6e509f0e9fa6d4f
Muhammad Munaf <43293019+muhammadmunaf@users.noreply.github.com>
2022-04-03 20:46:46 +0500

    Add files via upload

---
+++ TredCharts/script.js
@@ -0,0 +1,7 @@
+document.getElementById("loginPopUp").style.display = "none";
+
+function LoginPopUp() {
+    document.getElementById("loginPopUp").style.display = "flex";
+    console.log("opening");
+    //document.getElementsByTagName("body")[0].setAttribute("style", "backdrop-filter: blur(5px);")
+}
